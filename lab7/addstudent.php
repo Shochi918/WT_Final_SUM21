f2a00f4be2d3795f0dbcc173c0cd3a613a0085ec
Shochi918 <farjanai787@gmail.com>
2021-07-19 19:43:15 +0600

    lab7

--- lab7/addstudent.php
+++ lab7/addstudent.php
@@ -1,107 +1,41 @@
-<?php
-require_once "controllers/student-controller.php";
+<?php include 'admin_header.php';
+	require_once 'controllers/ProductController.php';
+	$products = getProducts();
 ?>
-<html>
-	<head>
-	</head> 
-	<body>
-	
-		<fieldset>
+<!--All student starts -->
+
+<div class="center">
+	<h3 class="text">All Student</h3>
+	<table class="table table-striped">
+		<thead>
+			<th>Sl#</th>
+			<th> Name</th>
+			<th>DOB </th>
+			<th> Credit</th>
+			<th> CGPA</th>
+			<th></th>
+			<th></th>
 			
-			<center><legend><h1>Add Student</h1></legend></center>
-			<center>
-			<form action="" method="post">
-				<table>
-					
-					
-					
-					
-					<tr>
-						<td><span> Name</span></td> 
-						<td>: <input type="text" value="<?php echo $name;?>" name="name">
-						<span><?php echo $err_name;?></span></td>
+		</thead>
+		<tbody>
+			<?php
+				$i=1;
+				foreach($student as $p){
+					echo "<tr>";
 						
-					</tr>
-					
-					
-					<tr>
-						<td>Date of Birth</td>
-						<td>:
-							<select>
-								<option>Day</option>
-								
-								<?php
-									for($i=1;$i<=31;$i++){
-										echo "<option>$i</option>";	
-									}
-								?>
-							</select>
-							<select>
-								<option>Month</option>
-								
-								<?php
-								    $month = array("January","February","March","April","May","June","July","August","September","October","November","December");
-									foreach($month as $v){
-										echo "<option>$v</option>";
-										
-									}
-								?>
-							</select>
-							<select>
-								<option>Year</option>
-								
-								<?php
-									for($i=1900;$i<=2021;$i++){
-										echo "<option>$i</option>";	
-									}
-								?>
-							</select>
-							
-						</td>
-					</tr>
-					
-					
-					
-				
-					<tr>
-						<td>Credit</td>
-						<td>: <input type="password" value="<?php echo $credit;?>" name="credit">
-						<span><?php echo $err_credit;?></span></td>
-					</tr>
-					
-				
-					
-					<tr>
-						<td>Cgpa</td>
-						<td>: <input type="text" value="<?php echo $cgpa;?>" name="cgpa">
-						<span><?php echo $err_cgpa;?></span></td>
-					</tr>
-					
-					<tr>
-						<td> Department</td>
-						<td>: <select>
-								<option>Choose</option>
-								
-								<?php
-								    $department = array("CSE","EEE","BBA");
-									foreach($department as $v){
-										echo "<option>$v</option>";
-										
-									}
-								?>
-							</select></td>
 						
-					</tr>
-					
-					<tr>
-						<td align="center" colspan="2"><input type="submit" name="submit" value="Add Student"></td>
-					</tr>
-					
-				</table>
-				 
-				
-			</form>
-			</center>
-		</fieldset>
-</body>
-</html>
+						echo "<td>".$p["name"]."</td>";
+						echo "<td>".$p["cgpa"]."</td>";
+						echo "<td>".$p["credit"]."</td>";
+						echo "<td>".$p["dob"]."</td>";
+						echo '<td><a href="edit_student.php?id='.$p["id"].'" class="btn btn-success">Edit</a></td>';
+						echo '<td><a class="btn btn-danger">Delete</td>';
+					echo "</tr>";
+					$i++;
+				}
+			?>
+		</tbody>
+	</table>
+</div>
+<!--Student ends -->
+<?php include 'main_footer.php';?>
--- lab7/dashboard.php
+++ lab7/dashboard.php
@@ -1,14 +1,52 @@
-<html>
-<head>
-
-</head>
-<body>
-<a href="allstudents.php">All Students</a>
-<a href="addstudent.php">Add Student</a>
-<a href="alldepartments.php">All Departments</a>
-<a href="adddepartment.php">Add Department</a>
-
-
-
-</body>
-</html>
+<?php include 'admin_header.php';?>
+<!--dashboard starts -->
+<div>
+    <table  align="center">
+        <tr>
+            <td>
+                <div class="card">
+                <span class="text-white"> Total Students <br>
+                    1000
+                </span>
+                </div>
+            </td>
+            <td>
+                <div class="card">
+                <span class="text-white"> Total Students <br>
+                    1000
+                </span>
+                </div>
+            </td>
+            <td>
+                <div class="card">
+                <span class="text-white"> Total Students <br>
+                    1000
+                </span>
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
+<div class="center">
+    <h3 class="text">Student List</h3>
+    <table class="table table-striped">
+        <thead>
+            <th>Sl#</th>
+            <th> Name</th>
+            <th>DOB</th>
+            <th>Credit</th>
+            <th>CGPA</th>
+            <th>DEPT_ID</th>
+        </thead>
+        <tbody>
+            <td>1</td>
+            <td>Shochi</td>
+            <td>5</td>
+            <td>14</td>
+            <td>3.5</td>
+            <td>19-40235-1</td>
+        </tbody>
+    </table>
+</div>
+<!--dashboard ends -->
+<?php include 'admin_footer.php';?>
